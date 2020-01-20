8824d66634ee192b8c2fa3bc400a6804f67ccb17
JoshiRaez <joseraezrodriguez@gmail.com>
2020-01-20 04:45:22 +0100

    Refactorized for vowel cases

--- src/main/java/PigLatinTranslator.java
+++ src/main/java/PigLatinTranslator.java
@@ -1,32 +1,35 @@
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Stream.of;
 
 public class PigLatinTranslator {
 
+    private final Predicate<String> STARTS_WITH_VOWEL
+            = Pattern.compile("^[aeiou]").asPredicate();
+    private final Predicate<String> STARTS_WITH_YT
+            = Pattern.compile("^yt").asPredicate();
+    private final Predicate<String> STARTS_WITH_XR
+            = Pattern.compile("^xr").asPredicate();
+
     public String translate(final String toTranslate) {
 
-        if (wordBeginsWithAnyVowel(toTranslate)
-                || wordBeginsWithYt(toTranslate)
-                || wordBeginsWithXr(toTranslate))
+        if (anyOf(
+                testNormalisingToLowerCaseThat(toTranslate, STARTS_WITH_VOWEL),
+                testNormalisingToLowerCaseThat(toTranslate, STARTS_WITH_YT),
+                testNormalisingToLowerCaseThat(toTranslate, STARTS_WITH_XR)
+        ))
             return toTranslate + "ay";
 
         return null;
     }
 
-    private boolean wordBeginsWithAnyVowel(String toTest) {
-        return Pattern.compile("^[aeiou]")
-                .asPredicate()
-                .test(toTest.toLowerCase());
+    private boolean testNormalisingToLowerCaseThat(final String word, final Predicate<String> matcher) {
+        return matcher.test(word.toLowerCase());
     }
 
-    private boolean wordBeginsWithYt(final String toTest) {
-        return Pattern.compile("^[yt]")
-                .asPredicate()
-                .test(toTest.toLowerCase());
-    }
-
-    private boolean wordBeginsWithXr(final String toTest) {
-        return Pattern.compile("^[xr]")
-                .asPredicate()
-                .test(toTest.toLowerCase());
+    private boolean anyOf(Boolean... conditions) {
+        return asList(conditions).contains(true);
     }
 }

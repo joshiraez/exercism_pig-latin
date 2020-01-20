d3c3074c89911bb246762b6b7ebaef2f82574111
JoshiRaez <joseraezrodriguez@gmail.com>
2020-01-20 18:08:04 +0100

    Refactorized to eliminate complexity on having to know regexes have to be normalized to underscore.

--- src/main/java/PigLatinTranslator.java
+++ src/main/java/PigLatinTranslator.java
@@ -2,34 +2,35 @@ import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Stream.of;
 
 public class PigLatinTranslator {
 
     private final Predicate<String> STARTS_WITH_VOWEL
-            = Pattern.compile("^[aeiou]").asPredicate();
+            = Pattern.compile("^[aeiouAEIOU]").asPredicate();
     private final Predicate<String> STARTS_WITH_YT
-            = Pattern.compile("^yt").asPredicate();
+            = Pattern.compile("^[yY][tT]").asPredicate();
     private final Predicate<String> STARTS_WITH_XR
-            = Pattern.compile("^xr").asPredicate();
+            = Pattern.compile("^[xX][rR]").asPredicate();
 
     public String translate(final String toTranslate) {
 
         if (anyOf(
-                testNormalisingToLowerCaseThat(toTranslate, STARTS_WITH_VOWEL),
-                testNormalisingToLowerCaseThat(toTranslate, STARTS_WITH_YT),
-                testNormalisingToLowerCaseThat(toTranslate, STARTS_WITH_XR)
+                testThat(toTranslate, STARTS_WITH_VOWEL),
+                testThat(toTranslate, STARTS_WITH_YT),
+                testThat(toTranslate, STARTS_WITH_XR)
         ))
             return toTranslate + "ay";
+
 
         return null;
     }
 
-    private boolean testNormalisingToLowerCaseThat(final String word, final Predicate<String> matcher) {
-        return matcher.test(word.toLowerCase());
+    private boolean testThat(final String word, final Predicate<String> matcher) {
+        return matcher.test(word);
     }
 
     private boolean anyOf(Boolean... conditions) {
         return asList(conditions).contains(true);
     }
+
 }

c9f7a332312b05e30692d29515b115c07d4c0a4a
JoshiRaez <joseraezrodriguez@gmail.com>
2020-01-20 04:16:16 +0100

    Refactored to use regex

--- src/main/java/PigLatinTranslator.java
+++ src/main/java/PigLatinTranslator.java
@@ -1,11 +1,17 @@
+import java.util.regex.Pattern;
+
 public class PigLatinTranslator {
 
     public String translate(final String toTranslate) {
 
-        if(toTranslate.startsWith("a")) return toTranslate + "ay";
-        if(toTranslate.startsWith("e")) return toTranslate + "ay";
-        if (toTranslate.startsWith("i")) return toTranslate + "ay";
+        if (wordBeginsWithAorEorI(toTranslate)) return toTranslate + "ay";
 
         return null;
+    }
+
+    private boolean wordBeginsWithAorEorI(String toTest) {
+        return Pattern.compile("^[aei]")
+                .asPredicate()
+                .test(toTest);
     }
 }
